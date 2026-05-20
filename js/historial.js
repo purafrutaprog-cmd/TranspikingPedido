3f30d85ed76db91fcc4ad98b96e75423a1ca5ece
purafrutaprog-cmd <purafrutaprog@gmail.com>
2026-05-20 09:22:44 +0200

    Update historial.js

--- js/historial.js
+++ js/historial.js
@@ -1,1 +1,48 @@
+async function cargarHistorial(){
 
+  const { data, error } = await supabase
+    .from("pedidos")
+    .select("*")
+    .order("id", { ascending:false });
+
+  if(error){
+    console.log(error);
+    return;
+  }
+
+  let html = `
+    <table>
+      <thead>
+        <tr>
+          <th>Pedido</th>
+          <th>Cliente</th>
+          <th>Fecha</th>
+          <th>Total</th>
+          <th>Factura</th>
+        </tr>
+      </thead>
+      <tbody>
+  `;
+
+  data.forEach(p => {
+
+    html += `
+      <tr>
+        <td>${p.numero_pedido || ""}</td>
+        <td>${p.cliente_nombre || ""}</td>
+        <td>${p.created_at || ""}</td>
+        <td>${eur(p.total || 0)}</td>
+        <td>
+          ${p.requiere_factura ? "SI" : "NO"}
+        </td>
+      </tr>
+    `;
+  });
+
+  html += `
+      </tbody>
+    </table>
+  `;
+
+  document.getElementById("historialTabla").innerHTML = html;
+}
