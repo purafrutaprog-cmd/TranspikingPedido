d14d995c9327c74e31b5d9ef2b4a4bbb1c51e7a9
purafrutaprog-cmd <purafrutaprog@gmail.com>
2026-05-27 00:30:20 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 	
 var SUPABASE_URL = "https://usvkjpugaccroerbnqva.supabase.co";
-var SUPABASE_KEY = "sb_publishable_WVtMN54rqEBatc_ZEAhVUw_oN5F8nFm";
+var SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzdmtqcHVnYWNjcm9lcmJucXZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTU3MjksImV4cCI6MjA5MzgzMTcyOX0.O8hIxkd62Aw9JaHpRFrbqL0TdM1MjdNxM1D3MsbQucE";
 
 var supabase = window.supabase.createClient(	
   SUPABASE_URL,
